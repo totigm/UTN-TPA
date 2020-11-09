c9523eba6bc9901aeda736401b1ca0e8e384f773
Hernán Cano <hernancano2014@gmail.com>
2020-11-09 16:23:37 -0300

    Update

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -2,12 +2,6 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("bucket");
-
-// Export the name of the bucket
-export const bucketName = bucket.id;
-
 // Step 1: Create an ECS Fargate cluster.
 const cluster = new awsx.ecs.Cluster("cluster");
 
