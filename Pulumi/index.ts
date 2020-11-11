1e9b9b4f1726742f4e775f09e47b3f4494bb8116
Gonzalo Muñoz <gonzalo.munoz@southworks.com>
2020-11-11 18:18:56 -0300

    test

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -1,37 +1,6 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-// import * as awsx from "@pulumi/awsx";
 
-// Fetch the default VPC information from your AWS account:
 const vpc = new aws.ec2.Vpc('VPCTPA', { cidrBlock: '10.0.0.0/16', enableDnsHostnames: true });
 
-// // Step 1: Create an ECS Fargate cluster.
-// const cluster = new awsx.ecs.Cluster("Cluster");
-
-// // Step 2: Define the Networking for our service.
-// const alb = new awsx.lb.ApplicationLoadBalancer(
-//     "net-lb", { external: true, securityGroups: cluster.securityGroups });
-// const web = alb.createListener("web", { port: 80, external: true });
-
-// // Step 3: Build and publish a Docker image to a private ECR registry.
-// const img = awsx.ecs.Image.fromPath("app-img", "./app");
-
-// // Step 4: Create a Fargate service task that can scale out.
-// const appService = new awsx.ecs.FargateService("app-svc", {
-//     cluster,
-//     taskDefinitionArgs: {
-//         container: {
-//             image: img,
-//             cpu: 102 /*10% of 1024*/,
-//             memory: 50 /*MB*/,
-//             portMappings: [ web ],
-//         },
-//     },
-//     desiredCount: 5,
-// });
-
-// Step 5: Export the Internet address for the service.
-// export const url = web.endpoint.hostname;
-
-// Export a few interesting fields to make them easy to use:
 export const vpcId = vpc.id;
