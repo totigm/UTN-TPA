6b93f085d7c620d7bb79bcb77e536549cc22de4f
Hernán Cano <hernancano2014@gmail.com>
2020-11-11 18:27:48 -0300

    test

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -3,4 +3,4 @@ import * as aws from "@pulumi/aws";
 
 const vpc = new aws.ec2.Vpc('VPCTPA', { cidrBlock: '10.0.0.0/16', enableDnsHostnames: true });
 
-export const vpcId = vpc.id; 
+export const vpcId = vpc.id;
