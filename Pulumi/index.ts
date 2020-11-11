21b602db28764a888f14bc7fcae968877a089690
Gonzalo Muñoz <gonzalo.munoz@southworks.com>
2020-11-11 18:21:53 -0300

    test

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -1,5 +1,6 @@
 import * as aws from "@pulumi/aws";
 
-const vpc = new aws.ec2.Vpc('tpa', { cidrBlock: '10.0.0.0/16', enableDnsHostnames: true });
+const s3 = new aws.s3.Bucket('test')
+// const vpc = new aws.ec2.Vpc('tpa', { cidrBlock: '10.0.0.0/16', enableDnsHostnames: true });
 
-export const vpcId = vpc.id;
+// export const vpcId = vpc.id;
