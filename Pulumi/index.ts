f2e9880f3c80c6eb6d14b5026477447fafb8dab1
Enzito <enzoavena1@gmail.com>
2020-11-21 15:43:37 -0300

    test

--- Pulumi/index.ts
+++ Pulumi/index.ts
@@ -2,22 +2,18 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
-// Step 1: Create an ECS Fargate cluster.
+// Paso 1: Creamos un ECS Fargate cluster.
 const cluster = new awsx.ecs.Cluster("cluster");
 
-// Step 2: Define the Networking for our service.
+// Paso 2: Definimos la conexion para nuestro servicio.
 const alb = new awsx.lb.ApplicationLoadBalancer(
     "net-lb", { external: true, securityGroups: cluster.securityGroups });
 const web = alb.createListener("web", { port: 80, external: true });
 
-//const repository = new awsx.ecr.Repository("repo");
-
-//const image = repository.buildAndPushImage("./app")
-
-// Step 3: Build and publish a Docker image to a private ECR registry.
+// Paso 3: Creamos y publicamos una imagen de Docker a un registro ECR privado.
 const img = awsx.ecs.Image.fromPath("app-img", "./app");
 
-//Step 4: Create a Fargate service task that can scale out.
+// Paso 4: Creamos un servicio Fargate que podemos poner a escala.
 const appService = new awsx.ecs.FargateService("app-svc", {
     cluster,
     taskDefinitionArgs: {
@@ -31,7 +27,5 @@ const appService = new awsx.ecs.FargateService("app-svc", {
     desiredCount: 5,
 });
 
-// Step 5: Export the Internet address for the service.
- export const url = web.endpoint.hostname;
-
-//export const vpcId = vpc.id; 
+// Paso 5: Exporta la URL del servicio.
+export const url = web.endpoint.hostname;
